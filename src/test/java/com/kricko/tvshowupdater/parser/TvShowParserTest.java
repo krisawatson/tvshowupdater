a6c2aac2b35151869c341359504701c4755ea617
krisawatson <treble00_01@homtail.com>
2022-02-11 20:20:14 +0000

    Unit tested TvParserTest

--- src/test/java/com/kricko/tvshowupdater/parser/TvShowParserTest.java
+++ src/test/java/com/kricko/tvshowupdater/parser/TvShowParserTest.java
@@ -2,14 +2,11 @@ package com.kricko.tvshowupdater.parser;
 
 import com.kricko.tvshowupdater.model.Details;
 import com.kricko.tvshowupdater.model.Shows;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 /**
  * The class <code>TvShowParserTest</code> contains tests for the class <code>{@link TvShowParser}</code>.
@@ -19,54 +16,15 @@ import static org.junit.Assert.assertNotNull;
  * @version $Revision: 1.0 $
  */
 public class TvShowParserTest {
-	/**
-	 * Run the Shows parseShows() method test.
-	 *
-	
-	 *
-	 * @generatedBy CodePro at 28/10/14 09:04
-	 * @throws Exception */
-	@Test(expected = java.io.FileNotFoundException.class)
-	public void testParseShows_1()
-		throws Exception {
-		TvShowParser fixture = new TvShowParser();
-
-		Shows result = fixture.parseShows();
-
-		// add additional test code here
-		assertNotNull(result);
-	}
 
 	/**
 	 * Run the Shows parseShows() method test.
 	 *
-	
-	 *
-	 * @generatedBy CodePro at 28/10/14 09:04
-	 * @throws Exception */
-	@Test(expected = java.io.FileNotFoundException.class)
-	public void testParseShows_2()
-		throws Exception {
-		TvShowParser fixture = new TvShowParser();
-
-		Shows result = fixture.parseShows();
-
-		List<Details> details = result.getShows();
-		// add additional test code here
-		assertNotNull(details);
-	}
-
-
-
-	/**
-	 * Run the Shows parseShows() method test.
-	 *
-
 	 *
 	 * @generatedBy CodePro at 28/10/14 09:04
 	 * @throws Exception */
 	@Test
-	public void testParseShowRegex() throws Exception {
+	public void testParseShow() throws Exception {
 		TvShowParser fixture = new TvShowParser();
 
 		Shows result = fixture.parseShows();
@@ -78,34 +36,6 @@ public class TvShowParserTest {
 		assertEquals("Brooklyn Nine Nine", details.get(0).getName());
 		assertEquals("Brooklyn Nine Nine", details.get(0).getRegexName());
 		assertEquals( "269586", details.get(0).getTvdbSeriesId().orElseGet(null));
-	}
-
-	/**
-	 * Perform pre-test initialization.
-	 *
-	
-	 *
-	 * @generatedBy CodePro at 28/10/14 09:04
-	 * @throws Exception
-	 *         if the initialization fails for some reason */
-	@Before
-	public void setUp()
-		throws Exception {
-		// add additional set up code here
-	}
-
-	/**
-	 * Perform post-test clean-up.
-	 *
-	
-	 *
-	 * @generatedBy CodePro at 28/10/14 09:04
-	 * @throws Exception
-	 *         if the clean-up fails for some reason */
-	@After
-	public void tearDown()
-		throws Exception {
-		// Add additional tear down code here
 	}
 
 	/**
